20ee251cab5bd906dafbf0b1bc37558e8cdd7344
pmanoelm <pmanoelm@everis.com>
2020-11-27 12:40:28 -0300

    Autor : Paulo Versão:0.1.0 Subindo pacotes iniciais

---
+++ LojaDeArtigosGerais/src/main/java/br/com/pmanoelm/Loja/LojaDeArtigosGerais/LojaDeArtigosGeraisApplication.java
@@ -0,0 +1,13 @@
+package br.com.pmanoelm.Loja.LojaDeArtigosGerais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeArtigosGeraisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeArtigosGeraisApplication.class, args);
+	}
+
+}
